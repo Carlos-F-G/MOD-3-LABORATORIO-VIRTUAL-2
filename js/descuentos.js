fa11ef3fc2b3c39ffaf55eeb58d9a8de864dd423
Carlos Farias Galdames <165774803+Carlos-F-G@users.noreply.github.com>
2024-12-09 12:34:47 -0300

    Update descuentos.js

--- js/descuentos.js
+++ js/descuentos.js
@@ -1,7 +1,7 @@
 import { calcularCostoTotal } from "./calculadoraServicios.js";
 
 // Función para aplicar un descuento
-const aplicarDescuento = (costo) => costo * 0.9; // 10% descuento
+const aplicarDescuento = (costo) => costo * 0.9;
 
 // Composición de funciones
 const calcularCostoFinal = (numeroDeConsultas, precioPorConsulta) =>
