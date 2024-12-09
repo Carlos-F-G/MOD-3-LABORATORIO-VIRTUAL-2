ae7e45de4a42d2628e87a7c4b9378d9c14e6f89a
Carlos Farias Galdames <165774803+Carlos-F-G@users.noreply.github.com>
2024-12-09 12:36:31 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// Importaciones correctas desde la misma carpeta
+
 import { manejarMenu } from "./menu.js";
 import { manejarModal } from "./modal.js";
 import { manejarBusqueda } from "./busqueda.js";
 import { manejarFormulario } from "./formulario.js";
 
-// Ejecutar las funcionalidades
+
 document.addEventListener("DOMContentLoaded", () => {
   manejarMenu();
   manejarModal();
