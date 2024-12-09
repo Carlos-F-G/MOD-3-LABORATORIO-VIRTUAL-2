9a42cc91fafe29c82f059e56eec12e2efed75bf7
Carlos Farias Galdames <165774803+Carlos-F-G@users.noreply.github.com>
2024-12-09 12:37:19 -0300

    Update tiempoEspera.js

--- js/tiempoEspera.js
+++ js/tiempoEspera.js
@@ -1,6 +1,5 @@
-// Función flecha para calcular el promedio de espera
 const calcularPromedioEspera = (tiempos) =>
   tiempos.reduce((acc, curr) => acc + curr, 0) / tiempos.length;
 
-// Exportar la función
+
 export { calcularPromedioEspera };
