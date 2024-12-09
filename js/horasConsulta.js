0ceb6ccc964b6444ceef50ca27951a2cc8a949bb
Carlos Farias Galdames <165774803+Carlos-F-G@users.noreply.github.com>
2024-12-09 12:36:16 -0300

    Update horasConsulta.js

--- js/horasConsulta.js
+++ js/horasConsulta.js
@@ -1,8 +1,8 @@
-// Recursión para calcular el total de horas de consulta
+
 const calcularHorasTotales = (horas, index = 0, total = 0) => {
   if (index >= horas.length) return total;
   return calcularHorasTotales(horas, index + 1, total + horas[index]);
 };
 
-// Exportar la función
+
 export { calcularHorasTotales };
